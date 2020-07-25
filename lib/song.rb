88a4e36c33d8f64bef7be31eb2a4a54ffa828110
Yuiko Grace Nakano <gracenak@gmail.com>
2020-07-25 04:39:02 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -54,9 +54,9 @@ class Song
   def self.create_from_filename(filename)
     create_filename = filename.split(" -")
     artist_name = create_filename[0]
-    name = create_filename[1].include("-.mp3")
+    new_name = create_filename[1].include("-.mp3")
     song = self.new
-    song.name = name
+    song.new_name = name
     song.artist_name = artist_name
     @@all << create_filename
   end
